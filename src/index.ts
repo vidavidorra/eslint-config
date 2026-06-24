50b5adc6a3a6932eca323cbfe0da392382672649
Jeroen de Bruijn <vv9239@gmail.com>
2026-06-24 21:17:25 +0200

    fix: also update eslint

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,17 @@
-import xo from 'xo';
+import eslintConfigXo from 'eslint-config-xo';
 import {type Linter} from 'eslint';
 
-const config: Linter.Config[] = xo.xoToEslintConfig([
-  {space: true, prettier: 'compat'},
-]);
+const config: Linter.Config[] = [
+  ...eslintConfigXo({
+    space: true,
+    prettier: 'compat',
+  }).filter(({name}) => name !== 'xo/jsdoc' && name !== 'xo/jsdoc-typescript'),
+  {
+    files: ['**/package-lock.json'],
+    rules: {
+      'json/no-empty-keys': 'off',
+    },
+  },
+];
 
 export default config;
