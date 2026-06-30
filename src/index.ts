2ebea4d7bcc953f142f0c136c0d8e2ce37c494d3
Jeroen de Bruijn <vv9239@gmail.com>
2026-06-30 07:16:23 +0200

    fix: disable `unicorn/consistent-boolean-name`
    I already consider naming of variables and am not a fan of the restrictions.

--- src/index.ts
+++ src/index.ts
@@ -16,6 +16,9 @@ const config: Linter.Config[] = [
     files: ['**/CHANGELOG.md'],
     rules: {'markdown/no-duplicate-headings': 'off'},
   },
+  {
+    rules: {'unicorn/consistent-boolean-name': 'off'},
+  },
 ];
 
 export default config;
